b498f3c9d7fe9a456b393175c3a2ade8fa51dbb1
luqiuyuan <lsun100@uottawa.ca>
2019-04-17 16:21:50 -0400

    First commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+img = mpimg.imread("dataset/training/2.jpg")
+print(img.shape)
+
+# imgplot = plt.imshow(img)
+# plt.show()
 
