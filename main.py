e61ce66e3b0a31b6d08b62795a25f34551f3bf90
luqiuyuan <lsun100@uottawa.ca>
2019-04-20 08:36:16 -0400

    Add testing data

--- main.py
+++ main.py
@@ -7,13 +7,25 @@ DIMENSION_INPUT = 49152
 NUM_ITERATIONS = 100
 m = 10 # Number of samples
 alpha = 0.001 # Learning rate
+SIZE_TESTING = 10 # Size of testing dataset
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-def readY():
+# Read inputs
+def readInputs(path):
+    X = np.zeros((DIMENSION_INPUT, 0))
+    for filename in glob.glob(path):
+        img = mpimg.imread(filename)
+        X = np.hstack((X, img.reshape((DIMENSION_INPUT, 1))))
+    # Normalize X so that it is in range [0, 1]
+    X = X / 128 - 1
+    return X
+
+# Read ground truth
+def readGroundTruth(path):
     Y = np.zeros((1, 0))
-    file = open("./dataset/training/data.txt", "r")
+    file = open(path, "r")
     for line in file:
         Y = np.hstack((Y, np.array([[int(line)]])))
     file.close()
@@ -39,23 +51,52 @@ def calculateBackwardPropagation(X, Y, A):
 def calculateCost(Y, A):
     return -(Y.dot(np.log(A.T)) + (1 - Y).dot(np.log(1 - A.T))) / m
 
-# Construct input matrix X
-X = np.zeros((DIMENSION_INPUT, 0))
-for filename in glob.glob("./dataset/training/*.jpg"):
-    img = mpimg.imread(filename)
-    X = np.hstack((X, img.reshape((DIMENSION_INPUT, 1))))
-# Normalize X so that it is in range [0, 1]
-X = X / 128 - 1
+######################
+# Training phase
+######################
 
-Y = readY()
+# Read inputs and ground truth for training phase
+X = readInputs("./dataset/training/*.jpg")
+Y = readGroundTruth("./dataset/training/data.txt")
 
 # (w, b) = initializeParametersZeros(DIMENSION_INPUT)
 (w, b) = initializeParametersRandom(DIMENSION_INPUT)
 
+Js = np.zeros((0))
 for i in range(0, NUM_ITERATIONS):
     A = calculateForwardPropagatation(X, w, b)
     J = calculateCost(Y, A)[0, 0]
-    print(J)
+    Js = np.hstack((Js, np.array([J])))
     (dw, db) = calculateBackwardPropagation(X, Y, A)
     w = w - alpha * dw
     b = b - alpha * db
+
+# Plot
+fig = plt.figure()
+plt.plot(np.arange(100), Js, label="Cost")
+plt.xlabel("Iterations")
+plt.ylabel("Cost")
+plt.title("Training")
+plt.show()
+
+######################
+# Testing phase
+######################
+
+# Read inputs and ground truth for testing phase
+X = readInputs("./dataset/training/*.jpg")
+Y = readGroundTruth("./dataset/testing/data.txt")
+
+# Calculate the forward propagation for the testing dataset using trained parameters
+A = calculateForwardPropagatation(X, w, b)
+Y_hat = np.copy(A)
+Y_hat[A>0.5] = 1
+Y_hat[A<=0.5] = 0
+
+# Calculate the precision
+num_corrects = 0
+for i in range(0, SIZE_TESTING):
+    if Y[0, i] == Y_hat[0, i]:
+        num_corrects += 1
+precision = num_corrects / SIZE_TESTING
+print("The precision of testing is " + str(precision*100) + "%.")
